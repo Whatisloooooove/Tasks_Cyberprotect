3547ac50c7c2e6d7ef97a486314274ef344cea8e
whatislove <morochkovskii.va@phystech.edu>
2025-02-24 23:34:07 +0300

    added realization

---
+++ dz3/README.md
@@ -0,0 +1,16 @@
+# Celsium and Kelvin
+
+## Описание
+Здесь реализованы псевдонимы Celsium и Kelvin и функционал работы с ними
+
+## Архитектура
+
+1. Функции для преобразования Celsium к Kelvin и наоборот: CelsiumToKelvin(Celsium) Kelvin и KelvinToCelsium(Kelvin) Celsium
+2. SwapTemparatures(*Celsium, *Kelvin) - функция для смены местами значений
+
+
+## Как запускать
+
+```
+go run main.go
+```
---
+++ dz3/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Celsium float64
+type Kelvin float64
+
+func CelsiumToKelvin(cel Celsium) Kelvin {
+	return Kelvin(cel + 273.15)
+}
+
+func KelvinToCelsium(kel Kelvin) Celsium {
+	return Celsium(kel - 273.15)
+}
+
+func SwapTemperatures(kelPtr *Kelvin, celPtr *Celsium) {
+	temp_kel := KelvinToCelsium(*kelPtr)
+	*kelPtr = CelsiumToKelvin(*celPtr)
+	*celPtr = temp_kel
+}
+
+func main() {
+	kelPtr := new(Kelvin)
+	*kelPtr = 315.92
+	celPtr := new(Celsium)
+	*celPtr = 20.0
+
+	SwapTemperatures(kelPtr, celPtr)
+	fmt.Printf("%.2f\n", *celPtr)
+	fmt.Printf("%.2f\n", *kelPtr)
+}
